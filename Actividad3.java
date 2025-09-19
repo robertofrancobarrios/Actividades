18135be3eac1efed757de0315431114a152cbccd
robertofrancobarrios <franco.barrios.roberto@iescamas.es>
2025-09-19 18:40:45 +0200

    Add files via upload

---
+++ Actividad3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Actividad3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Dime tu edad: ");
+		int edad = sc.nextInt();
+		System.out.println("La edad del usuario será: " + (edad+1));
+	}
+
+}
